5288c16e18473d7a9d76adef2a0de80c2d76ba19
Giovanni <gio.vannnni@hotmail.com>
2024-12-20 17:09:18 -0300

    fix: navbar updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/react";
 import { Route, Routes}  from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignInPage.js";
-import Navbar from "./components/Navbar.jsx";
 import LogInPage from "./pages/LogInPage.jsx";
 
 function App() {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import { Container, Flex, HStack, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { ColorModeButton } from "./ui/color-mode.jsx";
+import "../global.css";
+import logo from "../assets/icon.svg";  // Altere o caminho conforme necessário
 
 
 const Navbar = () => {
 
     return (
-        <Container maxW={"100%"} px={4} bg={{base: "var(--mauve)", _dark: "var(--purple)"}}>
+        <Container p="0" width="100vw" maxW="100vw" px={33} bg={{base: "var(--mint-green)", _dark: "var(--mint-green)"}}>
             <Flex
                 h={16}
                 alignItems={"center"}
@@ -17,16 +19,23 @@ const Navbar = () => {
                 }}
             >
                 <HStack spaceX={2}>
+                    <Link to={"/"}>
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        width={50}
+                        height={50}
+                    />
+                    </Link>
                     <Text
                         fontSize={"2xl"}
                         fontWeight={"bold"}
-                        fontFamily={"heading"}
-                        color={"var(--phlox)"}
+                        className="nabla-font"
                     >
                         <Link to={"/"}>IFB Share</Link>
                     </Text>
                 </HStack>
-                <ColorModeButton />
+                <ColorModeButton/>
             </Flex>
         </Container>
     );
