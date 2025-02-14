983ca37be9b7051b560fa1a88b013f11647d1ac2
Giovanni <gio.vannnni@hotmail.com>
2025-02-14 00:38:54 -0300

    fix: adding environment variable at vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-        '/api': {
-            target: "http://localhost:5000",
+    plugins: [react(), tsconfigPaths()],
+    server: {
+        proxy: {
+            '/api': {
+                target: import.meta.env.VITE_API_URL || "http://localhost:5000",
+                changeOrigin: true,
+                secure: false,
+            },
         },
+        port: 3000,
     },
-    port: 3000,
-  },
 });
