d4183fa555bdbf0ad0d3dc294e4637589991a7b9
Giovanni <gio.vannnni@hotmail.com>
2025-02-14 00:45:37 -0300

    fix: fixing deployment to vercel

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from 'dotenv';
 
-export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    server: {
-        proxy: {
-            '/api': {
-                target: import.meta.env.VITE_API_URL || "http://localhost:5000",
-                changeOrigin: true,
-                secure: false,
+export default defineConfig(({ mode }) => {
+    dotenv.config();
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react(), tsconfigPaths()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL || "http://localhost:5000",
+                    changeOrigin: true,
+                    secure: false,
+                },
             },
+            port: 3000,
         },
-        port: 3000,
-    },
+    };
 });
