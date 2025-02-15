e340f1bcd66bca89859fb8a16eace480f6d62359
Giovanni <gio.vannnni@hotmail.com>
2025-02-15 09:06:27 -0300

    fix: returning to original

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from "vite-tsconfig-paths";
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths"
 
-export default defineConfig(({ mode }) => {
-    dotenv.config();
-
-
-    return {
-        plugins: [react(), tsconfigPaths()],
-        server: {
-            proxy: {
-                '/api': {
-                    target: "https://ifbshare-back.onrender.com",
-                    changeOrigin: true,
-                    secure: false,
-                },
-            },
-            port: 3000,
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+        '/api': {
+            target: "http://localhost:5000",
         },
-    };
+    },
+    port: 3000,
+  },
 });
