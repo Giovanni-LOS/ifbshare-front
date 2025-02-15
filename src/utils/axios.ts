c793116158f2cc39300ccf8bcbdc81e430aee6c0
Giovanni <gio.vannnni@hotmail.com>
2025-02-15 15:42:45 -0300

    fix: deploy changes

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,23 +2,26 @@ import axios from "axios";
 import { globalRouter } from "./globalRouter";
 import { toaster } from "@/components/ui/toaster";
 
+// Define se as requisições devem enviar credenciais
 axios.defaults.withCredentials = true;
 
+// Configura o baseURL usando a variável de ambiente ou fallback para localhost
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+axios.defaults.baseURL = API_URL;
+
 axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response) {
-      const { status } = error.response;
+    (response) => response,
+    (error) => {
+        if (error.response) {
+            const { status } = error.response;
 
-      if (status === 401 || status === 403) {
-        toaster.create({ description: error.response?.message, title: 'Error', type: "error" });
-
-        globalRouter.navigate?.("/login");
-      }
+            if (status === 401 || status === 403) {
+                toaster.create({ description: error.response?.message, title: 'Error', type: "error" });
+                globalRouter.navigate?.("/login");
+            }
+        }
+        return Promise.reject(error);
     }
-
-    return Promise.reject(error);
-  }
 );
 
 export default axios;
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from 'dotenv';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-        '/api': {
-            target: "http://localhost:5000",
+export default defineConfig(({ mode }) => {
+    // Carregar as variáveis de ambiente corretamente
+    dotenv.config();
+    // eslint-disable-next-line no-undef
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react(), tsconfigPaths()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL || "http://localhost:5000",
+                    changeOrigin: true,
+                    secure: false,
+                },
+            },
+            port: 3000,
         },
-    },
-    port: 3000,
-  },
+    };
 });
