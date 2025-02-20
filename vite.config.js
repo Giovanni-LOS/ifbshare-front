c0ee208a00cfc1c6ab824d6c9acf13430c6adfc4
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-02-20 19:14:20 -0300

    feat: update Vite config to load environment variables from .env files

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import dotenv from "dotenv";
+import process from 'process';
 
-dotenv.config();
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
+      port: 3000,
     },
-    port: 3000,
-  },
+  };
 });
