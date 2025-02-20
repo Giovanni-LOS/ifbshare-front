5c231e872728ee18c5f7ca9a426b811cd3dc65ca
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-02-20 19:29:50 -0300

    fix: fallback vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import process from 'process';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), tsconfigPaths()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
       },
-      port: 3000,
     },
-  };
+    port: 3000,
+  },
 });
